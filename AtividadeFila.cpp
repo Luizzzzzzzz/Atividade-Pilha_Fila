95898f2d5b70cfdf36997fb5f16c5f7bb7c3730e
Luiz Otavio <luizotaviolr13@gmail.com>
2025-09-22 09:40:19 -0300

    finalização fila

--- AtividadeFila.cpp
+++ AtividadeFila.cpp
@@ -1,91 +1,101 @@
-// Fila com Inserção, Remoção, Mostrar toda fila, Cheio/Vazio, Implementação Dinâmica, localização de elemento,
+// Fila com Insercao, Remocao, Mostrar toda fila, Cheio/Vazio, Implementacao Dinamica, localizacao de elemento,
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 
 
-typedef int CHAVE;
+typedef int CHAVE; 
 
 typedef struct {
-    CHAVE chave;
-} REGISTRO;
+        CHAVE chave;
+} REGISTRO; 
 
 typedef struct aux {
-    REGISTRO reg;
-    struct aux* prox;
-} ELEMENTO;
+        REGISTRO reg;
+        struct aux* prox;
+} ELEMENTO; 
 
-typedef ELEMENTO* PONT;
+typedef ELEMENTO* PONT; 
 
-// Estrutura da fila
-typedef struct {
-    PONT inicio;
-    PONT fim;
-} FILA;
+typedef struct{
+        PONT inicio;
+		PONT fim;       
+}FILA; 
 
-// Inicializa fila
-bool inicializarFila(FILA* f) {
-    f->inicio = NULL;
-    f->fim = NULL;
-    
-    if(f->inicio==NULL && f->fim==NULL)
+bool inicializarFila (FILA* f) {
+     f->inicio = NULL;
+     f->fim = NULL;
+     
+    if(f->inicio == NULL && f->fim == NULL)
     return true;
     else
     return false;
 }
 
-// Verifica se a fila está vazia
-bool filaVazia(FILA* f) {
-    return (f->inicio == NULL);
+bool filaVazia (FILA* f) {
+     return (f->inicio == NULL); 
 }
 
-// Inserção no fim
-bool inserirNaFila(FILA* f, REGISTRO reg) {
-    PONT novo = (PONT) malloc(sizeof(ELEMENTO));
-    if (!novo) return false; // erro de alocação
-    novo->reg = reg;
-    novo->prox = NULL;
-
-    if (f->fim) { // se já existe fim
-        f->fim->prox = novo;
-    } else { // se a fila estava vazia
-        f->inicio = novo;
-    }
-    f->fim = novo;
-    return true;
+void mostrarFila(FILA* f) {
+	
+	system("cls");
+	if(filaVazia(f)){
+		printf("Fila Vazia");
+		return;
+	}
+	PONT atual = f->inicio;
+	printf("Fila: \n");	
+	
+	while (atual!=NULL){
+		printf("%i\n", atual->reg.chave); 
+		atual = atual->prox;
+	}
+     
 }
 
-// Remoção do início
-bool removerDaFila(FILA* f, REGISTRO* reg) {
-    if (filaVazia(f)) return false;
+bool inserirFila(FILA* f, REGISTRO reg) {
+    
+     PONT novo = (PONT) malloc(sizeof(ELEMENTO));
+     if(novo==NULL)
+     return false;
+     
+     novo->reg=reg;
+     novo->prox=NULL;
+     
+     if(f->fim) {
+     	f->fim->prox=novo;
+	 } else {
+	 	f->inicio=novo;
+	 }
+     
+     f->fim=novo;
+     return true;
+} 
+
+bool removerFila(FILA* f, REGISTRO* reg) {
+    if (filaVazia(f)) 
+		return false;
+		
     PONT apagar = f->inicio;
     *reg = apagar->reg;
     f->inicio = apagar->prox;
-    if (f->inicio == NULL) { // se esvaziou
-        f->fim = NULL;
-    }
+    if (f->inicio == NULL) f->fim = NULL;
     free(apagar);
     return true;
 }
 
-// Mostrar todos os elementos da fila
-void exibirFila(FILA* f) {
-    if (filaVazia(f)) {
-        printf("Fila vazia.\n");
-        return;
-    }
-    PONT atual = f->inicio;
-    printf("Fila: ");
-    while (atual != NULL) {
-        printf("%d ", atual->reg.chave);
+int searchElemento(FILA* f, CHAVE chave) {
+	PONT atual = f->inicio;
+	int lugar = 0;
+	
+	while (atual != NULL) {
+        if (atual->reg.chave == chave) 
+			return lugar;
+			
         atual = atual->prox;
+        lugar++;
     }
-    printf("\n");
-}
-
-int menu(int resposta){
-	
-	
+    return -1;
 }
 
 int main (){
@@ -94,27 +104,51 @@ int main (){
 	
 	int init = inicializarFila(&f);
 	if(init==true)
-	printf("Inicialização bem sucedida!\n");
+	printf("Inicializacao bem sucedida!\n");
 	else
-	printf("Inicialização Falha =()\n");
+	printf("Inicializacao Falha =()\n");
 	
 	printf("Quantos elementos deseja inserir?\n");
 	scanf("%d", &num_element);
 	printf("\n");
+	
 	while(numAux<num_element){
 		REGISTRO reg;
 		
 		printf("Digite o valor do %i elemento:\n", numAux+1);
 		scanf("%d", &reg.chave);
 		
-		inserirNaFila(&f, reg);
+		inserirFila(&f, reg);
 		numAux++;
 	}
 	system("cls");
-	printf("Inserção bem sucedida!\n");
+	printf("Insercao bem sucedida!\n");
 	
-	printf("Digite:\n1-Mostrar Topo\n2-Remover no\n");
+	printf("Digite:\n1-Mostrar Fila\n2-Remover no\n3-Procurar item\n");
 	scanf("%d", &resposta);
+	
+	switch(resposta){
+		case 1:
+			mostrarFila(&f);
+			break;
+		case 2: 
+		REGISTRO removido;
+			removerFila(&f, &removido);
+			printf("Elemento removido: %d\n", removido.chave);
+			mostrarFila(&f);
+			break;
+		case 3:
+			int chave;
+			system("cls");
+            printf("Digite o valor a localizar: \n");
+            scanf("%d", &chave);
+            int pos = searchElemento(&f, chave);
+            if (pos == -1) 
+				printf("Elemento nao encontrado.\n");
+            else 
+				printf("Elemento encontrado na %d posicao:\n", pos+1);
+            break;
 	}
+}
 	
 
